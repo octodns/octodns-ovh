beb5daefd67283b5364d0b4930aa6e82f982cc3e
Ross McFarland <rwmcfa1@gmail.com>
2023-11-11 12:41:30 -0800

    Add __version__ and deprecate __VERSION__ to line up with pep8 recommendation

--- octodns_ovh/__init__.py
+++ octodns_ovh/__init__.py
@@ -13,7 +13,8 @@ from ovh import ResourceNotFoundError
 from octodns.provider.base import BaseProvider
 from octodns.record import Record
 
-__VERSION__ = '0.0.2'
+# TODO: remove __VERSION__ with the next major version release
+__version__ = __VERSION__ = '0.0.2'
 
 
 class OvhProvider(BaseProvider):
